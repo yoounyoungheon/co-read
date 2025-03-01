e5a410c1b3249622b08992a7b9a5ba5b11ea33e9
yoounyoungheon <iddyoon@gmail.com>
2025-03-01 16:42:57 +0900

    [refactor] 프로젝트 초기화

--- web/src/app/(routes)/sign-up/layout.tsx
+++
@@ -1,8 +0,0 @@
-// page.tsx에서 렌더링한 파일들을 보냄
-export default function Layout({children}:{children: React.ReactNode;}){
-  return (
-    <div>
-      {children}
-    </div>
-  );
-}
--- web/src/app/ui/components/view/molecule/form-select/index.tsx
+++
@@ -1,8 +0,0 @@
-import { FormSelectRoot } from './form-select-root';
-import { FormSelectItem } from './form-select-item';
-
-const FormSelect = Object.assign(FormSelectRoot, {
-  Item: FormSelectItem,
-});
-
-export default FormSelect;
