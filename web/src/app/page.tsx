70a140cd120a535ef48dc92b1f4ce4ea2afa0b6c
yoounyoungheon <tbvjgjsl9@naver.com>
2025-04-11 16:28:16 +0900

    [feat] ui css 수정

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -7,6 +7,7 @@ import AboutMe from "@/app/assets/aboutme.png"
 import Project from "@/app/assets/project.png"
 import Article from "@/app/assets/article.png"
 import Link from "next/link";
+import Image from "next/image"
 
 
 export default async function MainPage() {
@@ -18,14 +19,15 @@ export default async function MainPage() {
       <div className="mb-6 text-center text-3xl text-blue-950 font-bold">{`Younghun's Log`}</div>
       <div className="mb-5 text-center text-xl text-blue-950 font-semibold">{`Welocme! This is Younghun's portfolio web`}</div>
       <div className="px-12 grid grid-cols-1 gap-5">
-        <Dialog>
+          <div className="grid grid-cols-1 gap-5 xl:grid-cols-3 lg:grid-cols-3 base:grid-cols-3 sm:grid-cols-3">
+          <Dialog>
             <DialogTrigger asChild>
               <Card
-                className="relative p-10 border-none text-center shadow-lg rounded-lg hover:scale-105 transition-transform duration-300 bg-cover bg-center"
-                style={{ backgroundImage: `url(${AboutMe.src})` }}
+                className="aspect-square relative p-10 mt-3 border-none text-center shadow-lg rounded-lg hover:scale-105 transition-transform duration-300 bg-cover bg-center"
               >
+                <Image src={AboutMe.src} alt={""} fill className="rounded-lg"/>
                 <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
-                <div className="relative flex items-center justify-center space-x-2 text-white text-xl font-semibold">
+                <div className="relative flex items-center justify-center space-x-2 text-white text-2xl font-semibold">
                   <span>ABOUT ME</span>
                 </div>
                 <div className="mt-3 relative text-white italic">간단한 소개글입니다.</div>
@@ -37,15 +39,13 @@ export default async function MainPage() {
                 <div>서버 오류입니다. 잠시만 기다려주세요.</div>}
             </DialogContent>
           </Dialog>
-          <div className="grid grid-cols-1 gap-5 xl:grid-cols-2 lg:grid-cols-2 base:grid-cols-2 sm:grid-cols-2">
-          
           <Link href={`/project`}>
             <Card
-              className="relative p-10 border-none text-center shadow-lg rounded-lg hover:scale-105 transition-transform duration-300 bg-cover bg-center"
-              style={{ backgroundImage: `url(${Project.src})` }}
+              className="aspect-square relative p-10 mt-3 border-none text-center shadow-lg rounded-lg hover:scale-105 transition-transform duration-300 bg-cover bg-center"
             >
+              <Image src={Project.src} alt={""} fill className="rounded-lg"/>
               <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
-              <div className="relative flex items-center justify-center space-x-2 text-white text-xl font-semibold">
+              <div className="relative flex items-center justify-center space-x-2 text-white text-2xl font-semibold">
                 <span>PROJECT</span>
               </div>
               <div className="mt-3 relative text-white italic">진행했던 프로젝트를 정리한 공간입니다.</div>
@@ -54,11 +54,11 @@ export default async function MainPage() {
 
           <Link href={`/article`}>
             <Card
-              className="relative p-10 border-none text-center shadow-lg rounded-lg hover:scale-105 transition-transform duration-300 bg-cover bg-center"
-              style={{ backgroundImage: `url(${Article.src})` }}
+              className="aspect-square relative p-10 mt-3 border-none text-center shadow-lg rounded-lg hover:scale-105 transition-transform duration-300 bg-cover bg-center"
             >
+              <Image src={Article.src} alt={""} fill className="rounded-lg"/>
               <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
-              <div className="relative flex items-center justify-center space-x-2 text-white text-xl font-semibold">
+              <div className="relative flex items-center justify-center space-x-2 text-white text-2xl font-semibold">
                 <span>ARTICLE</span>
               </div>
               <div className="mt-3 relative text-white italic">공부하고 고민했던 내용을 기록한 공간입니다.</div>
