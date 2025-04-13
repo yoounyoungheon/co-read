5404ecd32afef6cf73be508ef89a4841900b0352
yoounyoungheon <tbvjgjsl9@naver.com>
2025-04-13 18:32:56 +0900

    [ui] ui 변경

--- web/src/app/(routes)/layout.tsx
+++ web/src/app/(routes)/layout.tsx
@@ -1,9 +1,6 @@
-import { Nav } from "../ui/components/view/nav/nav";
-
 export default function Layout({ children }: { children: React.ReactNode; }) {
   return (
     <div>
-      <Nav/>
       <div className="p-4">{children}</div>
     </div>
   );
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from "./utils/style/fonts";
+import { Nav } from "./ui/components/view/nav/nav";
 
 export const metadata: Metadata = {
   title: "iamyounghun",
@@ -11,6 +12,7 @@ export default function RootLayout({children}:{children: React.ReactNode;}){
   return (
     <html lang="en">
       <body className={`${inter.className}, bg-slate-100`}>
+        <Nav/>
         {children}
       </body>
     </html>
