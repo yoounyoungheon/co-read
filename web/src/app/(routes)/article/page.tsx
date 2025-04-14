b0c05bb16088d89b49cfc5b7f38d5aa11b69cb6e
yoounyoungheon <tbvjgjsl9@naver.com>
2025-04-14 23:52:35 +0900

    .

--- web/src/app/(routes)/article/page.tsx
+++ web/src/app/(routes)/article/page.tsx
@@ -3,7 +3,13 @@ import { Card } from "@/app/ui/components/view/molecule/card/card";
 
 export default async function ArticlePage() {
   const loadArticlesResponse = await loadAllArticles();
-  console.log(loadArticlesResponse);
+
+  const sortedArticles = loadArticlesResponse.isSuccess && loadArticlesResponse.data
+    ? loadArticlesResponse.data.sort((a, b) => {
+        return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+      })
+    : [];
+
   return (
     <main>
       <div className="text-center text-3xl font-bold p-5">ARTICLE</div>
@@ -11,32 +17,27 @@ export default async function ArticlePage() {
         {`"개발자로서 공부하고 고민했던 내용을 기록한 공간입니다."`}
       </div>
       <div className="px-10 grid grid-cols-1 gap-3 xl:grid-cols-3 lg:grid-cols-3 base:grid-cols-3 sm:grid-cols-2">
-        {loadArticlesResponse.isSuccess && loadArticlesResponse.data ? (
-        loadArticlesResponse.data.map((article, index) => {
-            const { title, description, url } = article;
-            return (
-              <Card
-                key={index}
-                className="flex flex-col text-start p-4 hover:shadow-lg duration-300"
+        {sortedArticles.map((article, index) => {
+          const { title, description, url } = article;
+          return (
+            <Card
+              key={index}
+              className="flex flex-col text-start p-4 hover:shadow-lg duration-300"
+            >
+              <div className="flex-grow flex flex-col gap-2">
+                <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
+                <p className="text-sm text-gray-600">{description}</p>
+              </div>
+              <a
+                href={url}
+                className="mt-auto inline-block text-blue-500 hover:text-blue-700 font-medium"
               >
-                <div className="flex-grow flex flex-col gap-2">
-                  <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
-                  <p className="text-sm text-gray-600">{description}</p>
-                </div>
-                <a
-                  href={url}
-                  className="mt-auto inline-block text-blue-500 hover:text-blue-700 font-medium"
-                >
-                  원문 보러가기 ➡️
-                </a>
-              </Card>
-
-            );
-        })
-        ) : (
-        <></>
-        )}
+                원문 보러가기 ➡️
+              </a>
+            </Card>
+          );
+        })}
       </div>
     </main>
-  )
+  );
 }
--- web/src/app/business/article/article.domain.ts
+++ web/src/app/business/article/article.domain.ts
@@ -8,9 +8,11 @@ export interface Article {
   description: string;
 
   url: string;
+
+  createdAt: Date;
 }
 
-export const createArticleDomain = (id: string, userId: string, title: string, description: string, url: string): Article => {
-  const article: Article = { id, userId, title, description, url };
+export const createArticleDomain = (id: string, userId: string, title: string, description: string, url: string, createdAt: string): Article => {
+  const article: Article = { id, userId, title, description, url, createdAt: new Date(createdAt) };
   return article;
 }
