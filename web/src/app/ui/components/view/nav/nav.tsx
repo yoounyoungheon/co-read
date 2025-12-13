144d845dae693a33e898d1847c773a297724ae9e
yoounyoungheon <tbvjgjsl9@naver.com>
2025-12-13 18:36:27 +0900

    ui

--- web/src/app/ui/components/view/nav/nav.tsx
+++ web/src/app/ui/components/view/nav/nav.tsx
@@ -2,26 +2,13 @@ import Link from "next/link";
 
 export function Nav() {
   return (
-    <div className=" text-blue-950 p-4 sm:p-1">
+    <div className=" text-blue-950 px-4 py-2 sm:p-1">
       <nav className="flex justify-between items-center px-1">
-        <div className="text-lg font-bold hidden sm:block">YounghunLog</div>
-        <ul className="flex space-x-6 font-semibold">
-          <li>
-            <Link href="/" className="hover:underline">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/" className="hover:underline">
-              Project
-            </Link>
-          </li>
-          <li>
-            <Link href="/" className="hover:underline">
-              Article
-            </Link>
-          </li>
-        </ul>
+        <div className="text-lg font-bold">
+          <Link href="/" className="no-underline">
+            iamyounghun
+          </Link>
+        </div>
       </nav>
     </div>
   );
