4bccb5b2b9bf9a808d2f5b269063552276668647
yoounyoungheon <tbvjgjsl9@naver.com>
2026-04-06 01:11:00 +0900

    modify profile

--- web/src/app/feature/profile/business/profile.service.ts
+++ web/src/app/feature/profile/business/profile.service.ts
@@ -12,7 +12,7 @@ export const loadProfileForGuestRequest = async (): Promise<
       id: "1",
       userId: "1",
       introduce:
-        "안녕하세요! 개발자 윤영헌입니다.\n융합소프트웨어와 경영학을 전공했습니다.\n비즈니스, 기술적 관점에서 변화에 유연한 소프트웨어 설계를 고민합니다.\n\n해당 웹 앱 github: https://github.com/yoounyoungheon/co-read\n",
+        "안녕하세요! 개발자 윤영헌입니다.\n융합소프트웨어와 경영학을 전공했습니다.\n비즈니스, 기술적 관점에서 변화에 유연한 소프트웨어 설계를 고민합니다.\n\ncurrent web app repo: https://github.com/yoounyoungheon/co-read\n",
       words: ["Dongguk Univ · scsc & biz", "Open Labs · 2025 ~"],
       gitUrl: "https://github.com/yoounyoungheon/co-read",
       blogUrl: "https://younghun123.tistory.com/",
